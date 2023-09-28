bc6b52502b321bdf12b95daceff3eb89443f6c44
ying <zycs19@gmail.com>
2023-09-28 12:02:06 -0700

    Login auth

--- sticky_notes/src/features/notes/NewNote.js
+++ sticky_notes/src/features/notes/NewNote.js
@@ -5,8 +5,9 @@ import NewNoteForm from './NewNoteForm.js'
 const NewNote = () => {
     const users = useSelector(selectAllUsers)
 
-    const content = users ? <NewNoteForm users={users} /> : <p>Loading...</p>
-
+    if(!users?.length) 
+        return <p>Not Currently Available</p>
+    const content = <NewNoteForm users = {users} />
     return content
 }
 export default NewNote
