00ad49f1776b890b5305d67343d42dd8a02401db
ying <zycs19@gmail.com>
2023-09-29 23:19:43 -0700

    roles and permissions

--- sticky_notes/src/components/DashFooter.js
+++ sticky_notes/src/components/DashFooter.js
@@ -1,7 +1,12 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faHouse } from "@fortawesome/free-solid-svg-icons"
 import { useLocation, useNavigate } from "react-router-dom"
+import useAuth from "../hooks/useAuth"
+
 const DashFooter = () => {
+
+    const {username, status} = useAuth()
+
     const navigate = useNavigate()
     const {pathname} = useLocation()
 
@@ -24,8 +29,8 @@ const DashFooter = () => {
     const content = (
         <footer className="dash-footer">
             {goHomeButton}
-            <p>Current User:</p>
-            <p>Status:</p>
+            <p>Current User: {username} </p>
+            <p>Status:{status} </p>
         </footer>
     )
   return content
